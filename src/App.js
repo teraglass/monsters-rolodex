99828948c74891e22d099bcaac6ae05cee6c94bb
teraglass <thecodss@gmail.com>
2021-06-29 15:57:36 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { Component } from 'react';
+import { SearchBox } from './components/search-box/search-box.component.jsx'
+ 
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      monsters : [],
+      searchField: ''
+    }
+    this.handelChange = this.handelChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters : users}))
+  } 
+
+  handelChange = e => {
+    this.setState({ searchField: e.target.value})
+  }
+  render(){
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      )
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handelChange= {this.handelChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+
+    );
+  }  
+}
+
+export default App;
